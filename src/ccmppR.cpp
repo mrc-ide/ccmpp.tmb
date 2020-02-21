a03e64104be8f809f42e8be3e99250a569aa9d7d
Jeff Eaton <jeffrey.eaton@imperial.ac.uk>
2020-02-21 14:20:31 +0000

    move template definition to .h

--- src/ccmppR.cpp
+++ src/ccmppR.cpp
@@ -2,40 +2,6 @@
 #include <RcppEigen.h>
 #include "ccmpp.h"
 
-template <class Type>
-Eigen::SparseMatrix<Type>
-make_leslie_matrix(const Eigen::Array<Type, Eigen::Dynamic, 1>& sx,
-                   const Eigen::Array<Type, Eigen::Dynamic, 1>& fx,
-                   const Type srb,
-                   const Type age_span,
-                   const int fx_idx) {
-  
-  Type fert_k = sx[0] * 0.5 * age_span / (1.0 + srb);
-
-  int fxd = fx.rows();
-  Eigen::Array<Type, Eigen::Dynamic, 1> fert_leslie(fxd + 1);
-  fert_leslie.setZero();
-  fert_leslie.block(0, 0, fxd, 1) += fx * sx.block(fx_idx, 0, fxd, 1);
-  fert_leslie.block(1, 0, fxd, 1) += fx;
-  fert_leslie *= fert_k;
-
-  int dim = sx.rows() - 1;
-  Eigen::SparseMatrix<Type> leslie(dim, dim);
-  leslie.reserve(Eigen::VectorXi::Constant(dim, 2));  // 2 non-zero entries per column
-
-  for (int i = 0; i < fxd+1; i++) {
-    leslie.insert(0, fx_idx + i - 1) = fert_leslie[i];
-  }
-
-  for (int i = 1; i < dim; i++) {
-    leslie.insert(i, i-1) = sx[i];
-  }
-  leslie.insert(dim-1, dim-1) = sx[dim];
-  leslie.makeCompressed();
-
-  return leslie;
-}
-
 //' Construct a sparse Leslie matrix
 //'
 //' @param sx a vector of survivorship probabilities.
