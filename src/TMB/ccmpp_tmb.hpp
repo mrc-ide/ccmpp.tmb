ddfbda1ac3aacfd8a4214fc1c918eeafa9a4a276
jeffeaton <jeffrey.eaton@imperial.ac.uk>
2020-02-22 21:05:34 +0000

    TMB likelihood for census counts

--- src/TMB/ccmpp_tmb.hpp
+++ src/TMB/ccmpp_tmb.hpp
@@ -17,13 +17,27 @@ Type ccmpp_tmb(objective_function<Type>* obj)
   DATA_SCALAR(age_span);
   DATA_INTEGER(fx_idx);
 
-  PARAMETER(theta);
+  // census data
+  DATA_MATRIX(census_log_pop);
+  DATA_IVECTOR(census_year_idx);
 
   matrix<Type> projpop(ccmpp<Type>(basepop, sx, fx, srb, age_span, fx_idx-1));
+    
+  Type nll(0.0);
+  
+  PARAMETER(log_sigma_logpop);
+  Type sigma_logpop(exp(log_sigma_logpop));
+  nll -= log_sigma_logpop; // change of variable
+
+  for(int i = 0; i < census_year_idx.size(); i++) {
+    nll -= dnorm(vector<Type>(census_log_pop.col(i)),
+		 log(vector<Type>(projpop.col(census_year_idx[i]))),
+		 sigma_logpop, true).sum();
+  }
 
   REPORT(projpop);
 
-  return Type(0);
+  return Type(nll);
 }
 
 #undef TMB_OBJECTIVE_PTR
--- tests/testthat/test-tmb.R
+++ tests/testthat/test-tmb.R
@@ -1,17 +1,21 @@
 test_that("TMB objective function returns results", {
 
+  data(burkina_faso_females, package = "popReconstruct")
+  
   data <- list(basepop = as.numeric(burkina.faso.females$baseline.pop.counts),
                sx = burkina.faso.females$survival.proportions,
                fx = burkina.faso.females$fertility.rates[4:10, ],
                srb = rep(1.05, ncol(burkina.faso.females$survival.proportions)),
                age_span = 5,
-               fx_idx = 4L)
-  par <- list(theta = 0)
+               fx_idx = 4L,
+               census_log_pop = log(burkina.faso.females$census.pop.counts),
+               census_year_idx = c(3L, 5L, 7L, 9L))
+  par <- list(log_sigma_logpop = 0)
   
   obj <- TMB::MakeADFun(data = c(model = "ccmpp_tmb", data), parameters = par,
                         DLL = "leapfrog_TMBExports", silent = TRUE)
   
-  expect_equal(obj$fn(), 0)
+  expect_true(is.finite(obj$fn()))
   expect_equal(obj$report()$projpop,
                ccmppR(data$basepop,
                       data$sx,
@@ -19,4 +23,5 @@ test_that("TMB objective function returns results", {
                       data$srb,
                       data$age_span,
                       data$fx_idx))
+
 })
